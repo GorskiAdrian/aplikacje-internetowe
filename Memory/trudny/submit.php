93b3a1b5f06d289974c7d1f47837859b4a3bbfb4
GorskiAdrian <105986579+GorskiAdrian@users.noreply.github.com>
2023-04-18 21:22:47 +0200

    Add files via upload

---
+++ Memory/trudny/submit.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+$wynik=$_POST['wynik'];
+
+$con=mysqli_connect('localhost','root','');
+mysqli_select_db($con,'ed_games');
+$reg="insert into memory(trudny_wynik,userLogin) values ('$wynik','{$_SESSION['username']}')";
+if(isset($_SESSION['username'])){
+mysqli_query($con,$reg);}
+header('location: http://localhost/inz/Memory/trudny/memory.php')
+
+
+?>
