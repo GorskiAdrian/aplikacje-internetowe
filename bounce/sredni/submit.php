44b03f1fefb38973167824d5958b9d69abd224ef
GorskiAdrian <105986579+GorskiAdrian@users.noreply.github.com>
2023-04-18 21:24:57 +0200

    Create submit.php

---
+++ bounce/sredni/submit.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+$wynik=$_POST['wynik'];
+
+$con=mysqli_connect('localhost','root','');
+mysqli_select_db($con,'ed_games');
+$reg="insert into bounce(sredni_wynik,userLogin) values ('$wynik','{$_SESSION['username']}')";
+if(isset($_SESSION['username'])){
+mysqli_query($con,$reg);}
+header('location: http://localhost/inz/Bounce/sredni/lekcja6.php')
+
+
+?>
